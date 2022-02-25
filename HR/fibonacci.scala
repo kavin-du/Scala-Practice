58cc1d97d910ab507b954a5572799b4c9c346109
Kavindu Chamith <kavindu.chamith@pagero.com>
2022-02-25 16:24:45 +0530

    fibonacci

--- HR/fibonacci.scala
+++ HR/fibonacci.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object Solution {
     
   // def fibonacci(x:Int):Int = x match {
@@ -6,21 +8,21 @@ object Solution {
   //     case _ => fibonacci(x-1) + fibonacci(x-2)
   // }
 
-  def fibonacci(x: Int): Int = x match {
+  def fibonacci(x: Int): Int = {
     @tailrec
-    def fibAcc(x: Int, acc: Int): Int = x match {
-      case 0 => 0
-      case 1 => 1
-      case _ => fibonacci(x-1) + fibonacci(x-2)
+    def fibAcc(x: Int, a: Int, b: Int): Int = x match {
+      case 0 => a
+      case 1 => b
+      case _ => fibAcc(x-1, b, a + b)
     }
-    
-    fibAcc(x, 0)
+
+    fibAcc(x, 0, 1)
   }
 
   def main(args: Array[String]) = {
     // println(fibonacci(readInt()))
 
-    for(i <- 0 to 5) printf("%d ", fibonacci(i))
+    for(i <- 0 to 10) printf("%d ", fibonacci(i))
 
   }
 }
